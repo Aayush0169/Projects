635015094549bbd2bca701d50d231a2ca743d96f
Aayush0169 <rajaayush124212@gmail.com>
2024-04-14 23:31:10 +0530

    First Commit

--- CALCULATOR/script.js
+++ CALCULATOR/script.js
@@ -4,7 +4,7 @@ function calculateTip() {
     var totalTip = billAmount * (tipPercentage / 100);
     var totalBill = parseFloat(billAmount) + totalTip;
   
-    document.getElementById('totalTip').textContent = 'Total Tip: $' + totalTip.toFixed(2);
-    document.getElementById('totalBill').textContent = 'Total Bill: $' + totalBill.toFixed(2);
+    document.getElementById('totalTip').textContent = 'Total Tip: rs' + totalTip.toFixed(2);
+    document.getElementById('totalBill').textContent = 'Total Bill: rs' + totalBill.toFixed(2);
   }
   
